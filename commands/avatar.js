3a40b5601b659426aa4ee84f89dbdcf9e2b36df4
Silver <79312009+SilverGamez@users.noreply.github.com>
2023-09-07 08:16:59 +1000

    Commit

---
+++ commands/avatar.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    data: {
+        name: "avatar",
+        description: "Shows a user avatar",
+        botdevonly: false
+    },
+    run: async (message, args, client) => {
+        let target = message.mentions.members.first();
+        if (!target) target = message.member;
+
+        const avatar = target.user.displayAvatarURL({
+            dynamic: true,
+            format: 'png'
+        });
+
+        const embed = new Discord.EmbedBuilder()
+            .setTitle(`${target.user.username}'s avatar`)
+            .setImage(avatar)
+            .setFooter({
+                text: message.guild.name,
+                iconURL: message.guild.iconURL()
+            })
+            .setColor('Blurple')
+
+        message.channel.send({
+            embeds: [embed]
+        });
+    }
+}
